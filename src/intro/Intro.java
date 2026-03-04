7fa4fe4d0be8c4ffab9860b3d68c1f8e4277f941
Md Imam Hussain <mihussain737@gmail.com>
2026-03-04 14:41:44 +0530

    all Date formats

--- src/intro/Intro.java
+++ src/intro/Intro.java
@@ -1,7 +1,18 @@
 package intro;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Intro {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		Date date =new Date();
+		System.out.println(date);	//Wed Mar 04 14:37:38 IST 2026
+		
+		LocalDateTime localDateTime=LocalDateTime.now();
+		System.out.println(localDateTime);	// 2026-03-04T14:37:39.089337900
+		
+		LocalDate localDate=LocalDate.now();
+		System.out.println(localDate);	//2026-03-04
 	}
 }
