668096347ce6410a0816f3f6d3790d24a144cbd0
Md Imam Hussain <mihussain737@gmail.com>
2026-03-05 21:43:58 +0530

    LocalTime added

--- src/localDateTime/LocalDateClass.java
+++ src/localDateTime/LocalDateClass.java
@@ -1,4 +1,4 @@
-package first;
+package localDateTime;
 import java.time.LocalDate;
 import java.time.Month;
 
---
+++ src/localDateTime/LocalTimeClass.java
@@ -0,0 +1,26 @@
+package localDateTime;
+
+import java.time.LocalTime;
+
+public class LocalTimeClass {
+	public static void main(String[] args) {
+		
+		LocalTime now=LocalTime.now();
+		System.out.println(now); 	// 15:33:42.549307100
+		
+		// Hour , Minute and Second
+		System.out.println(now.getHour());
+		System.out.println(now.getMinute());
+		System.out.println(now.getSecond());
+		
+		// Custom Time
+		LocalTime customTime=LocalTime.of(20, 55, 40); 
+		System.out.println(customTime);		// 20:55:40
+		
+		// Parsed Time from String to LocalTime
+		String time="15:30:40";
+		LocalTime parsedTime=LocalTime.parse(time);
+		System.out.println(parsedTime); 	//15:30:40
+	}
+
+}
